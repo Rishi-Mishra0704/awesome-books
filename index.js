1c0ec8536b2f75321cecc4d27fa0473fba36f605
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-02-20 15:55:18 +0530

    Add function to add books

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const form = document.querySelector('#form');
 const title = document.querySelector('#title');
-const author = document.querySelector('#author');
+const author = document.querySelector('#author')
 const addBtn = document.querySelector('#addBtn');
 const bookLocal = JSON.parse(localStorage.getItem('bookStr'));
+const bookContainer = document.getElementById('displayBook')
 
 
-addBtn.addEventListener('click',add);
+const newBooks = [{
+  title: title.value,
+  author: author.value
+}]
+
+addBtn.addEventListener('click',(event)=>{
+    newBooks.forEach((newBook) =>{
+        bookContainer.innerHTML = `
+      <p class = "title">${newBook.title}</p>  
+      <p class = "author">${newBook.author}</p>
+      <button type = "button" class = "remove" onClick = "remove()">remove</button>
+  `
+  console.log(newBook)
+  console.log(newBooks)
+    })
+    event.preventDefault()
+    
+})
