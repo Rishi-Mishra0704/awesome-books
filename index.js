f870e3b72a685c2bebd6a435ba73add8ec61825f
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-02-20 19:30:01 +0530

    fix some linter errors

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const form = document.querySelector('#form');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const addBtn = document.querySelector('#addBtn');
-const bookContainer = document.getElementById('displayBook')
+const bookContainer = document.getElementById('displayBook');
 let allBooks;
 
 const displayBooks = () => {
   if (localStorage.getItem('allBooks') == null) {
     allBooks = [];
-  }
-   else {
+  } else {
     allBooks = JSON.parse(localStorage.getItem('allBooks'));
   }
 
@@ -25,9 +24,9 @@ const displayBooks = () => {
 };
 
 const add = (Title, Author) => {
-    const idRandom = Math.floor(Math.random() * 100000);
-    allBooks.push({id: idRandom, title: Title, author: Author });
-    localStorage.setItem("allBooks", JSON.stringify(allBooks));
+  const idRandom = Math.floor(Math.random() * 100000);
+  allBooks.push({ id: idRandom, title: Title, author: Author });
+  localStorage.setItem('allBooks', JSON.stringify(allBooks));
   displayBooks();
 };
 
@@ -42,14 +41,12 @@ form.addEventListener('submit', () => {
 });
 
 const remove = (id) => {
-  const removedBooks = allBooks.filter(book => book.id === id);
-    if (removedBooks.length === 1) {
-      allBooks = allBooks.filter(book => book.id !== id);
-  localStorage.setItem('allBooks', JSON.stringify(allBooks));
-    }
-   window.location.reload();
+  const removedBooks = allBooks.filter((book) => book.id === id);
+  if (removedBooks.length === 1) {
+    allBooks = allBooks.filter((book) => book.id !== id);
+    localStorage.setItem('allBooks', JSON.stringify(allBooks));
   }
+  window.location.reload();
+};
 
-addBtn.addEventListener('click',displayBooks);
-
-
+addBtn.addEventListener('click', displayBooks);
