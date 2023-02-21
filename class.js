2954e32f79db6a61e59f043aa1bd39c8030957c4
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-02-21 18:53:51 +0530

    Convert website to single page application

--- class.js
+++ class.js
@@ -6,8 +6,32 @@ const form = document.querySelector('#form');
 const addBtn = document.querySelector('#addBtn');
 const bookContainer = document.getElementById('displayBook');
 const allBook = JSON.parse(localStorage.getItem('allBook')) || [];
+const contact = document.querySelector('.contact-info');
+const contactBtn = document.getElementById('contact-btn');
 const { title, author } = form.elements;
+const listBtn = document.getElementById('list-btn');
+const addNewBtn = document.getElementById('add-new-btn');
+const addBookSection = document.getElementById('addBookSection');
+/* For Navigation */
 
+function navigation() {
+  contactBtn.addEventListener('click', () => {
+    contact.style.display = 'block';
+    bookContainer.style.display = 'none';
+    addBookSection.style.display = 'none';
+  });
+  listBtn.addEventListener('click', () => {
+    bookContainer.style.display = 'block';
+    addBookSection.style.display = 'none';
+    contact.style.display = 'none';
+  });
+  addNewBtn.addEventListener('click', () => {
+    addBookSection.style.display = 'flex';
+    bookContainer.style.display = 'none';
+    contact.style.display = 'none';
+  });
+}
+navigation();
 /*  for showing time and date */
 function updateTime() {
   const today = new Date();
