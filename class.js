9fcea94aecfb657eda59441956c8235692acbbb3
Abdus Sattar <sasorganicagro@gmail.com>
2023-02-23 00:04:01 +0600

    Fix reloading issues

--- class.js
+++ class.js
@@ -1,7 +1,6 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-unused-vars */
-
 const form = document.querySelector('#form');
 const addBtn = document.querySelector('#addBtn');
 const bookContainer = document.getElementById('displayBook');
@@ -51,6 +50,20 @@ setInterval(updateTime, 1000);
 
 /* for adding and removing books  */
 
+const displayBook = (books) => {
+  // clear the container
+  bookContainer.innerHTML = '';
+  books.forEach((book) => {
+    const div = document.createElement('div');
+    div.classList.add('items');
+    div.innerHTML = `
+        <p> "${book.title}" by <strong>${book.author}</strong></p>
+        <button onclick="library.remove(${book.id})" type="button" >Remove</button>
+        `;
+    bookContainer.appendChild(div);
+  });
+};
+
 class Book {
   constructor(id, title, author) {
     this.id = id;
@@ -69,6 +82,8 @@ class Library {
     const bookInfo = new Book(idRandom, title.value, author.value);
     this.books.push(bookInfo);
     localStorage.setItem('allBook', JSON.stringify(this.books));
+    // update the UI
+    displayBook(this.books);
   }
 
   remove(id) {
@@ -76,28 +91,21 @@ class Library {
     if (removedBooks.length === 1) {
       this.books = this.books.filter((book) => book.id !== id);
       localStorage.setItem('allBook', JSON.stringify(this.books));
+      // update the UI
+
+      displayBook(this.books);
     }
-    window.location.reload();
   }
 }
 
 const library = new Library();
-form.addEventListener('submit', () => {
+
+// update the UI on page load
+
+displayBook(allBook);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
   library.add(title, author);
+  form.reset();
 });
-
-const displayBook = (books) => {
-  books.forEach((book) => {
-    const title = book.title[0].toUpperCase() + book.title.slice(1);
-    const author = book.author[0].toUpperCase() + book.author.slice(1);
-    const div = document.createElement('div');
-    div.classList.add('items');
-    div.innerHTML = `
-        <p> "${title}" by <strong>${author}</strong></p>
-        <button onclick="library.remove(${book.id})" type="button" >Remove</button>
-        `;
-    bookContainer.appendChild(div);
-  });
-};
-
-addBtn.addEventListener('click', displayBook(allBook));
